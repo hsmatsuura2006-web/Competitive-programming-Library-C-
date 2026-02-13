125568ae7d6f913f6847a6b85db86f87aebbb891
shin0830 <h.s.matsuura.2006@gmail.com>
2026-02-13 14:01:42 +0900

    Update Segment Tree for Range Additions and Range Sums.cpp

--- Code Library/Tree/Segment Tree/Segment Tree for Range Additions and Range Sums.cpp
+++ Code Library/Tree/Segment Tree/Segment Tree for Range Additions and Range Sums.cpp
@@ -1,40 +1,50 @@
+#include <vector>
+#include <algorithm>
+
+template <typename T>
 struct SegmentTree {
 public:
-  ll n;
-  vector<ll> data, datb;
-  SegmentTree(int _n) {
-    n = 1;
-    while (n < _n) n <<= 1;
-    data.assign(2 * n, 0);
-    datb.assign(2 * n, 0);
-  }
-  void add(ll a, ll b, ll x) {
-    add(a, b, x, 0, 0, n);
-  }
-  ll sum(ll a, ll b) {
-    return sum(a, b, 0, 0, n);
-  }
-private:
-  void add(ll a, ll b, ll x, ll k, ll l, ll r) {
-    if (a <= l && r <= b) {
-      data[k] += x;
-    }else if (l < b && a < r) {
-      datb[k] += (min(b, r) - max(a, l)) * x;
-      add(a, b, x, k * 2 + 1, l, (l + r)/2);
-      add(a, b, x, k * 2 + 2, (l + r)/2, r);
-    }
-  }
+    int n;
+    std::vector<T> data, datb;
 
-  ll sum(ll a, ll b, ll k, ll l, ll r) {
-    if (b <= l || r <= a) {
-      return 0;
-    }else if (a <= l && r <= b) {
-      return data[k] * (r - l) + datb[k];
-    }else {
-      ll res = (min(b, r) - max(a, l)) * data[k];
-      res += sum(a, b, k * 2 + 1, l, (l + r)/2);
-      res += sum(a, b, k * 2 + 2, (l + r)/2, r);
-      return res;
+    SegmentTree(int _n) {
+        n = 1;
+        while (n < _n) n <<= 1;
+        data.assign(2 * n - 1, T(0));
+        datb.assign(2 * n - 1, T(0));
     }
-  }
+
+    void add(int a, int b, T x) {
+        if (a >= b) return;
+        add(a, b, x, 0, 0, n);
+    }
+
+    T sum(int a, int b) {
+        if (a >= b) return T(0);
+        return sum(a, b, 0, 0, n);
+    }
+
+private:
+    void add(int a, int b, T x, int k, int l, int r) {
+        if (a <= l && r <= b) {
+            data[k] += x;
+        } else if (l < b && a < r) {
+            datb[k] += T(std::min(b, r) - std::max(a, l)) * x;
+            add(a, b, x, k * 2 + 1, l, (l + r) / 2);
+            add(a, b, x, k * 2 + 2, (l + r) / 2, r);
+        }
+    }
+
+    T sum(int a, int b, int k, int l, int r) {
+        if (b <= l || r <= a) {
+            return T(0);
+        } else if (a <= l && r <= b) {
+            return data[k] * (r - l) + datb[k];
+        } else {
+            T res = T(std::min(b, r) - std::max(a, l)) * data[k];
+            res += sum(a, b, k * 2 + 1, l, (l + r) / 2);
+            res += sum(a, b, k * 2 + 2, (l + r) / 2, r);
+            return res;
+        }
+    }
 };
