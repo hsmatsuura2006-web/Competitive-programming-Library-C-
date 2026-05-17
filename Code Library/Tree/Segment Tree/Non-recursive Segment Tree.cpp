cc1290f9d8b1ae50e65d255ed777dc7df83c33c5
shin0830 <h.s.matsuura.2006@gmail.com>
2026-05-17 16:10:59 +0900

    Update Non-recursive Segment Tree.cpp

--- Code Library/Tree/Segment Tree/Non-recursive Segment Tree.cpp
+++ Code Library/Tree/Segment Tree/Non-recursive Segment Tree.cpp
@@ -18,6 +18,14 @@ struct SegmentTree {
             tree[i] = op(tree[i << 1], tree[i << 1 | 1]); 
         }
     }
+    void up(int i, T x) {
+        i += n;
+        tree[i] = x;
+        while (i > 1) {
+            i >>= 1; 
+            tree[i] = op(tree[i << 1], tree[i << 1 | 1]); 
+        }
+    }
     T query(int l, int r) {
         T res_l = e(), res_r = e();
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
